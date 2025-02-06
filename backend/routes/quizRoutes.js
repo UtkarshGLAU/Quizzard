ccdd30afb3c4626fc60d15ae3266c971d0b27387
UtkarshGLAU <122614349+UtkarshGLAU@users.noreply.github.com>
2025-02-06 12:13:00 +0530

    Initial_commit

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Quiz = require('../models/Quiz');
 router.get('/quizzes', async (req, res) => {
     const quizzes = await Quiz.find();
+    
     res.json(quizzes);
 });
 module.exports = router;
