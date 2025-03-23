80258c9bb3b0c86ca97e8d6495e5fe8696b1f094
Utkarsh Sharma <122614349+UtkarshSharma980@users.noreply.github.com>
2025-03-23 11:40:07 +0530

    "backend cors fixes"

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ const app = express()
 const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
-const cors = require("cors");
 
 app.use(cors({
     origin: ["https://quizzard-frontend.vercel.app", "http://localhost:5173"], // Allow both production and local frontend
