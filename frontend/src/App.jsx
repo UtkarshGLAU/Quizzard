362c5cae2586da0daf0f3c06f16bd3df4f1909b4
UtkarshGLAU <utkarsh.sharma_cs22@gla.ac.in>
2025-04-08 10:07:24 +0530

    "Timer&&RoutesLogin"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-import Login from './Login/Login'
-import Dashboard from './DashBoard/Dashboard'
-import NotFound from './NodeFound/NotFound';
-import Home from './Home/Home';
-import CreateQuiz from './CreateQuiz/CreateQuiz';
-import QuizPage from './QuizPage/QuizPage';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+import Dashboard from "./DashBoard/Dashboard";
+import NotFound from "./NodeFound/NotFound";
+import Home from "./Home/Home";
+import CreateQuiz from "./CreateQuiz/CreateQuiz";
+import QuizPage from "./QuizPage/QuizPage";
+import PublicRoute from "./components/PublicRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path="/quiz/:id" element={<QuizPage/>} />
-        <Route path="/create-quiz" element={<CreateQuiz/>} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/create-quiz"
+          element={
+            <ProtectedRoute>
+              <CreateQuiz />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/quiz/:id" element={<QuizPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
